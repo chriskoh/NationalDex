68dff5c620f0a27301cbe1f9134026654c3f4574
chriskoh <chris.koh90@gmail.com>
2014-01-15 17:18:04 -0800

    commit pklookup.py

---
+++ pklookup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+
+import requests
+import time
+import os
+from bs4 import BeautifulSoup as bs
+
+def main():
+
+        os.system("clear")
+
+        var = raw_input("Enter pokemon number (Three digit format): ")
+        url = "http://www.serebii.net/pokedex-xy/"+str(var)+".shtml"
+
+        r = requests.get(url)
+        soup = bs(r.text)
+
+        print("\n" + soup.title.text + "\n")
+
+        mod = soup.find_all(text=["*0", "*0.25", "*0.5", "*0.75", "*1", "*1.25", "*1.5", "*1.75", "*2"])
+        type = ['NRM', 'FIR', 'WTR', 'ELE', 'GRS', 'ICE', 'FGT', 'PSN', 'GND', 'FLY', 'PSY', 'BUG', 'RCK', 'GST', 'DRG', 'DRK', 'STL', 'FAR']
+
+        n = 0
+        for eachmod in mod:
+            print(type[n] + " " + eachmod)
+            n=n+1
+
+if __name__ == "__main__":
+    main()
